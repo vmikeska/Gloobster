f9f9df8f9bebac56a476604453549ac234def1a2
Vaclav <mikeska@gmail.com>
2015-10-26 20:59:24 +0100

    Upload file async base

--- src/Portal/wwwroot/_references.js
+++ src/Portal/wwwroot/_references.js
@@ -21,6 +21,7 @@
 /// <reference path="js/registration/facebookinit.js" />
 /// <reference path="js/registration/googlebutton.js" />
 /// <reference path="js/registration/loginmanager.js" />
+/// <reference path="js/uploadifive/jquery.uploadifive.min.js" />
 /// <reference path="js/views/friendsview.js" />
 /// <reference path="js/views/homepageview.js" />
 /// <reference path="js/views/pinboardview.js" />
