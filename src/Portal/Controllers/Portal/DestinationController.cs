a07f9d7ce68d2b58e64ac63dba670b736b73c790
Vaclav <mikeska@gmail.com>
2015-11-19 16:52:50 +0100

    Added Destination template

---
+++ src/Portal/Controllers/Portal/DestinationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gloobster.Database;
+using Gloobster.Entities;
+using Gloobster.Portal.Controllers.Base;
+using Gloobster.Portal.ViewModels;
+using Microsoft.AspNet.Mvc;
+using MongoDB.Bson;
+using System.IO;
+using Gloobster.Common;
+using Gloobster.DomainInterfaces;
+using Gloobster.DomainModels;
+using Gloobster.Entities.Trip;
+using Gloobster.Enums;
+
+namespace Gloobster.Portal.Controllers.Portal
+{
+	public class DestinationController : PortalBaseController
+	{
+		public DestinationController(IDbOperations db) : base(db)
+		{
+			
+		}
+
+		public IActionResult Planning()
+		{
+			var viewModel = CreateViewModelInstance<ViewModelPlanning>();
+
+			return View(viewModel);
+		}
+
+	}
+
+	
+
+}
