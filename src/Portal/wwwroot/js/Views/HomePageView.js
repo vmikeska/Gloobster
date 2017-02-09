7737552e85dbc91b354a1e69b6ecf59cbe335722
Vaclav <mikeska@gmail.com>
2017-02-09 13:33:07 +0100

    Title bar homepage

--- src/Portal/wwwroot/js/Views/HomePageView.js
+++ src/Portal/wwwroot/js/Views/HomePageView.js
@@ -9,10 +9,19 @@ var Views;
         __extends(HomePageView, _super);
         function HomePageView() {
             _super.call(this);
+            this.initSearch();
             Views.AirLoc.registerLocationCombo($("#currentCity"), function (place) {
                 window.location.href = "/deals";
             });
         }
+        HomePageView.prototype.initSearch = function () {
+            this.searchFrom = new Planning.DealsPlaceSearch($("#searchFrom"), "From place");
+            this.searchTo = new Planning.DealsPlaceSearch($("#searchTo"), "To place");
+            var tomorrow = moment().add(1, "days");
+            var in3Days = moment().add(3, "days");
+            this.dateFrom = new Common.MyCalendar($("#dateFrom"), tomorrow);
+            this.dateTo = new Common.MyCalendar($("#dateTo"), in3Days);
+        };
         Object.defineProperty(HomePageView.prototype, "pageType", {
             get: function () { return PageType.HomePage; },
             enumerable: true,
--- src/Portal/wwwroot/js/Views/HomePageView.ts
+++ src/Portal/wwwroot/js/Views/HomePageView.ts
@@ -6,11 +6,29 @@
 			constructor() {
 				super();
 
+					this.initSearch();
+
 				AirLoc.registerLocationCombo($("#currentCity"), (place) => {
 						window.location.href = "/deals";
 				});
 					
 			}
+
+			private searchFrom;
+			private searchTo;
+			private dateFrom;
+			private dateTo;
+
+				private initSearch() {
+						this.searchFrom = new Planning.DealsPlaceSearch($("#searchFrom"), "From place");
+						this.searchTo = new Planning.DealsPlaceSearch($("#searchTo"), "To place");
+
+						var tomorrow = moment().add(1, "days");
+						var in3Days = moment().add(3, "days");
+
+						this.dateFrom = new Common.MyCalendar($("#dateFrom"), tomorrow);
+						this.dateTo = new Common.MyCalendar($("#dateTo"), in3Days);
+				}
 
 			public get pageType(): PageType { return PageType.HomePage; }
 
